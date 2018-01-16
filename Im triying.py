9348f676c5d9919bc883d50d1441da6ae9098f77
Gustavasa <enystedt@gcds.net>
2018-01-15 21:00:14 -0500

    Update Im triying.py

--- Im triying.py
+++ Im triying.py
@@ -1,6 +1,3 @@
-import os
-os.makedirs('C:\\My Stuff\\')
-f= open("C:\\My Stuff\\Hello.py","w+")
-for i in range(10)
-    f.write("Hello is this working (i+1)")
-
+HelloFile = open('Hello.py', 'w')
+HelloFile.write('hello\n')
+HelloFile.close()
