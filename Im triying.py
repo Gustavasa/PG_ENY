9821595d86057115df132dac03aadfee58a2db5a
Gustavasa <enystedt@gcds.net>
2018-01-18 14:37:50 -0500

    Update Im triying.py

--- Im triying.py
+++ Im triying.py
@@ -1,3 +1,16 @@
+import pyautogui as pg, os, sys, time
 HelloFile = open('Hello.py', 'w')
-HelloFile.write('hello\n')
+HelloFile.write(
+"""
+import pyautogui as pg, os, sys, time
+pg.hotkey('winleft')
+pg.typewrite('shell\\n')
+pg.typewrite('shutdown -s')
+
+
+
+"""
+)
 HelloFile.close()
+
+import Hello.py
